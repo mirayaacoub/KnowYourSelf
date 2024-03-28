20622b7577c72ee44eedc64ffcd06213490b3d7c
Rabab Salim <rabab12j@gmail.com>
2024-03-29 01:33:13 +0200

    error handling ish attempt

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,24 +1,31 @@
-const { authenticate } = require('../services/authenticate.service');
-var jwt = require('jsonwebtoken');
-require('dotenv').config();
+/***
+ *auth.controller.js
+ ****/
+const { authenticate } = require("../services/authenticate.service");
+var jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const authenticateController = async (req, res) => {
-    const user = req.body;
+  const user = req.body;
 
-    if (!user) {
-        return res.status(401).json({ message: "Missing data" })
-    }
-    const result = await authenticate(user.user);
-    if (result.status === 200) {
-        const token = jwt.sign({ user_id: result?.user?.user_id }, process.env.SECRET_KEY)
-        console.log(result.message, result.user);
-        return res.status(200).json({ message: result.message, user: result.user, token: token })
-    }
-    console.log(result.message);
-    return res.status(401).json({ message: result.message });
-
-}
+  if (!user) {
+    return res.status(401).json({ message: "Missing data" });
+  }
+  const result = await authenticate(user.user);
+  if (result.status === 200) {
+    const token = jwt.sign(
+      { user_id: result?.user?.user_id },
+      process.env.SECRET_KEY
+    );
+    console.log(result.message, result.user);
+    return res
+      .status(200)
+      .json({ message: result.message, user: result.user, token: token });
+  }
+  console.log(result.message);
+  return res.status(401).json({ message: result.message });
+};
 
 module.exports = {
-    authenticateController,
-}
+  authenticateController,
+};
--- api/services/authenticate.service.js
+++ api/services/authenticate.service.js
@@ -1,23 +1,27 @@
-const { User } = require('../models/user.model');
-const bcrypt = require('bcrypt');
+/***
+ *authenticate.service.js
+ ****/
+
+const { User } = require("../models/user.model");
+const bcrypt = require("bcrypt");
 
 const authenticate = async (data) => {
-    const { email, password } = data;
-    
-    try {
-        const user = await User.findOne({where:{email: email}});
-        let correct = await bcrypt.compare(password, user.password);
-        // console.log(correct)
-        if (user && correct) {
-            return { status: 200, message: "Successful", user: user }
-        } else {
-            return { status: 401, message: "Cannot login with these credentials!" }
-        }
-    } catch (error) {
-        return { status: 500, message: "Internal error" }
+  const { email, password } = data;
+
+  try {
+    const user = await User.findOne({ where: { email: email } });
+    let correct = await bcrypt.compare(password, user.password);
+    // console.log(correct)
+    if (user && correct) {
+      return { status: 200, message: "Successful", user: user };
+    } else {
+      return { status: 401, message: "Cannot login with these credentials!" };
     }
-}
+  } catch (error) {
+    return { status: 500, message: "Internal error" };
+  }
+};
 
 module.exports = {
-    authenticate,
-}
+  authenticate,
+};
